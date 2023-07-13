f8a8d94ef6e98287e776ee7a0094f939ac1b04af
FraFra2 <kekkino41@gmail.com>
2023-07-13 10:50:31 +0200

    ADD hello world

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+      return {
+        
+      }
+    },
+    components: {
+      
+    }
+  }
+
+</script>
+
+<template>
+  <h1>
+    Hello World!
+  </h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
